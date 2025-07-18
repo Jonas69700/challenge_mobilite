ae2a5fec67d22a616f87b4938d133cc1763aed56
Jonas69700 <jonasflacher@gmail.com>
2025-07-18 21:20:48 +0200

    Fix: Création de seeder a la main, Railway ne prend pas en charge les fakers et factorys

--- database/seeders/ActivitySeeder.php
+++ database/seeders/ActivitySeeder.php
@@ -9,25 +9,57 @@ use Illuminate\Support\Collection;
 
 class ActivitySeeder extends Seeder
 {
+    // public function run(): void
+    // {
+    //     $users = User::all();
+
+    //     foreach ($users as $user) {
+    //         $dates = collect();
+
+    //         // Générer 5 à 10 activités par utilisateur
+    //         foreach (range(1, rand(5, 10)) as $_) {
+    //             // Éviter doublons par date
+    //             $date = now()->subDays(rand(0, 30))->format('Y-m-d');
+    //             if ($dates->contains($date)) continue;
+    //             $dates->push($date);
+
+    //             Activity::factory()->create([
+    //                 'user_id' => $user->id,
+    //                 'date' => $date,
+    //             ]);
+    //         }
+    //     }
+    // }
     public function run(): void
     {
-        $users = User::all();
+        $userIds = User::pluck('id')->toArray();
+        $types = ['bike', 'walk'];
+        $dates = [];
 
-        foreach ($users as $user) {
-            $dates = collect();
+        // Générer 40 activités réparties sur plusieurs jours/utilisateurs
+        for ($i = 0; $i < 40; $i++) {
+            // Sélectionner un user aléatoire
+            $userId = $userIds[array_rand($userIds)];
+            // Date entre aujourd'hui et il y a 30 jours
+            $date = now()->subDays(rand(0, 30))->format('Y-m-d');
+            // Pour limiter les doublons par user/date, tu peux garder une clé
+            $key = "$userId-$date";
+            if (isset($dates[$key])) continue;
+            $dates[$key] = true;
 
-            // Générer 5 à 10 activités par utilisateur
-            foreach (range(1, rand(5, 10)) as $_) {
-                // Éviter doublons par date
-                $date = now()->subDays(rand(0, 30))->format('Y-m-d');
-                if ($dates->contains($date)) continue;
-                $dates->push($date);
+            $type = $types[array_rand($types)];
+            $steps = $type === 'walk' ? rand(3000, 15000) : null;
+            $distance = $type === 'bike'
+                ? round(rand(100, 2500) / 100, 2) // entre 1 et 25km
+                : round($steps / 1500, 2);
 
-                Activity::factory()->create([
-                    'user_id' => $user->id,
-                    'date' => $date,
-                ]);
-            }
+            Activity::create([
+                'user_id' => $userId,
+                'date' => $date,
+                'type' => $type,
+                'steps' => $steps,
+                'distance_km' => $distance,
+            ]);
         }
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -9,19 +9,47 @@ use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
+    // public function run(): void
+    // {
+    //     $teams = Team::all();
+
+    //     User::factory()->create([
+    //         'name' => 'Admin',
+    //         'email' => 'admin@challenge.test',
+    //         'password' => Hash::make('admin123'),
+    //         'team_id' => $teams->random()->id,
+    //     ]);
+
+    //     User::factory(14)->create([
+    //         'team_id' => fn () => $teams->random()->id,
+    //     ]);
+    // }
+
     public function run(): void
     {
-        $teams = Team::all();
+        // Supposons que tu as 5 équipes (avec IDs 1 à 5)
+        $teams = [1, 2, 3, 4, 5];
+        $usersData = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@challenge.test',
+                'password' => Hash::make('admin123'),
+                'team_id' => 1,
+            ],
+        ];
 
-        User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@challenge.test',
-            'password' => Hash::make('admin123'),
-            'team_id' => $teams->random()->id,
-        ]);
+        // 14 utilisateurs supplémentaires répartis sur les équipes
+        for ($i = 1; $i <= 14; $i++) {
+            $usersData[] = [
+                'name' => "User $i",
+                'email' => "user$i@challenge.test",
+                'password' => Hash::make("password$i"),
+                'team_id' => $teams[($i - 1) % count($teams)],
+            ];
+        }
 
-        User::factory(14)->create([
-            'team_id' => fn () => $teams->random()->id,
-        ]);
+        foreach ($usersData as $user) {
+            User::create($user);
+        }
     }
 }
